ad4f9e29f58bc1aa407a0d45226aafb52e570732
Hrithvik <hrithvikatluri@gmail.com>
2024-06-11 15:33:54 +0530

    Created erroandler file

--- server.js
+++ server.js
@@ -2,11 +2,21 @@ const express=require('express')
 const app=express()
 const path=require('path')
 const {logger}=require('./middleware/logger')
+const errorHandler=require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+constcookieParser=require('cookie-parser')
+const cors=require('cors')
+const corsOptions=require("./config/corsOptions")
 const PORT=process.env.port || 3500
 
 app.use(logger)
 
+app.use(cors(corsOptions))
+
 app.use(express.json())
+
+app.use(cookieParser())
+
 app.use('/',express.static(path.join(__dirname,'/public')))
 
 //app.use(express.static('public'))
@@ -21,5 +31,7 @@ app.all('*',(req,res)=>{
         res.json({message : '404 : Page not found'})
     } else res.type('txt').send('404 : Page not found')
 })
+
+app.use(errorHandler)
 
 app.listen(PORT,()=>console.log(`server running on: ${PORT}`))
